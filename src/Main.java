86ff35f32c3cb7a2fb997eb2ddf8f5755534b163
Olga S <sangvis@list.ru>
2022-01-21 21:28:47 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        int ticketPrice = 2000;
+        int moneyForBonusMile = 20 * 1;
+
+        int bonusMile = ticketPrice / moneyForBonusMile ;
+
+        System.out.println("Количество бонусных милей за покупку билета - ");
+        System.out.println((bonusMile));
+
+        int receivedBonus = 100;
+    }
+}
